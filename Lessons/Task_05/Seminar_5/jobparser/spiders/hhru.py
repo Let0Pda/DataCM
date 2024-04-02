6ec5abd723e7609f82b856b7771272d0fc6bde64
Alexey Gavrilov <108893884+Let0Pda@users.noreply.github.com>
2024-04-02 19:54:58 +0300

    DZ5

---
+++ Lessons/Task_05/Seminar_5/jobparser/spiders/hhru.py
@@ -0,0 +1,28 @@
+import scrapy
+from scrapy.http import HtmlResponse
+from ..items import JobparserItem
+
+
+class HhruSpider(scrapy.Spider):
+    name = "hhru"
+    allowed_domains = ["hh.ru"]
+    start_urls = [
+        "https://hh.ru/search/vacancy?from=suggest_post&hhtmFrom=main&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&text=Python&enable_snippets=false&L_save_area=true"
+    ]
+
+    def parse(self, response: HtmlResponse):
+
+        next_page = response.xpath("//a[@data-qa='pager-next']/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+        links = response.xpath("//span/a[@class='bloko-link' and @target='_blank']/@href").getall()
+        for link in links:
+            yield response.follow(link, callback=self.vacansy_parse)
+
+    def vacansy_parse(self, response: HtmlResponse):
+        name = response.xpath("//h1/text()").get()
+        salary = response.xpath("//div[@data-qa='vacancy-salary']//text()").getall()
+        url = response.url
+        yield JobparserItem(name=name, salary=salary, url=url)
+        print()
