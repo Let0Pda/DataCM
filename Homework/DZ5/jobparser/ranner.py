e86bb581dad9d23a81fa8b17a5f5513ae5a9a08c
Alexey Gavrilov <108893884+Let0Pda@users.noreply.github.com>
2024-04-04 00:07:05 +0300

    ДЗ5_New

--- Homework/DZ5/jobparser/ranner.py
+++ Homework/DZ5/jobparser/ranner.py
@@ -2,6 +2,8 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.reactor import install_reactor
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
+
+# from spiders.hhru import HhruSpider
 from spiders.superjobru import SuperjobruSpider
 
 
@@ -10,5 +12,6 @@ if __name__ == "__main__":
     settings = get_project_settings()
     install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
     process = CrawlerProcess(settings)
+    # process.crawl(HhruSpider)
     process.crawl(SuperjobruSpider)
     process.start()
